3123105e0c528753771c07c5083523f744cb017d
shipleaf <shipleaf@shipleafui-MacBookPro.local>
2025-04-27 23:52:15 +0900

    0427 회의 수정본

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="mobile-web-app-capable" content="yes" />
-        <link rel="manifest" href="/manifest.json" />
-      </head>
       <body className={`${pretendard.variable} font-pretendard m-auto`}>
         <TanStackProvider>{children}</TanStackProvider>
       </body>
